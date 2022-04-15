160a4fc580a8e04c25cd6c3f1fda85237d35857b
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-15 12:28:41 +0530

    first commit

---
+++ index2.js
@@ -0,0 +1,31 @@
+const uName=document.querySelector('#name');
+const email=document.querySelector('#email');
+const btn=document.querySelector('.btn');
+const list=document.querySelector('#users');
+const msg=document.querySelector('.msg');
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    if(uName.value!='' && email.value!=''){
+        const li=document.createElement('li');
+        li.innerText=`${uName.value} ${email.value}`;
+        list.appendChild(li);
+        uName.value='';
+        email.value='';
+
+        list.firstElementChild.style.backgroundColor="green";
+        if(list.childNodes[1])
+            list.childNodes[1].style.backgroundColor="yellow";
+    }
+    else{
+        msg.innerText=`Please fill in the details`;
+        msg.classList.add('error');
+
+        setTimeout(()=>{
+            msg.innerText='';
+            msg.classList.remove('error');
+        },3000);
+    }
+
+});
