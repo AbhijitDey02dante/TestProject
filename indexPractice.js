e07f8e0d3b2fd3680fce01a394fd81d43fe8aa7b
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-15 22:09:50 +0530

    updated elements style by class

--- indexPractice.js
+++ indexPractice.js
@@ -1,6 +1,12 @@
 let header = document.getElementById("main-header");
 let addItem = document.getElementById("addItem");
 let headerTitle = document.getElementById("header-title");
+let listItems = document.getElementsByClassName('list-group-item');
+
+listItems[2].style.backgroundColor='green';
+for(let i of listItems){
+    i.style.fontWeight='bold';
+}
 // header.style.borderBottom="2px solid black";
 headerTitle.style.borderBottom="2px solid black";
 addItem.style.fontWeight="bold";
