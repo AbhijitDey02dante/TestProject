b1f6b09af8d2bc0d74f7551637f1480219c239fc
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-15 21:54:30 +0530

    new practice files

---
+++ indexPractice.js
@@ -0,0 +1,7 @@
+let header = document.getElementById("main-header");
+let addItem = document.getElementById("addItem");
+let headerTitle = document.getElementById("header-title");
+// header.style.borderBottom="2px solid black";
+headerTitle.style.borderBottom="2px solid black";
+addItem.style.fontWeight="bold";
+addItem.style.color="green";
