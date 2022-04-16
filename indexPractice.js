e994be7a6a6dd2dc1aa00842656078693d7261b0
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-16 09:35:02 +0530

    Delete Item function

--- indexPractice.js
+++ indexPractice.js
@@ -1,72 +1,33 @@
-// let header = document.getElementById("main-header");
-// let addItem = document.getElementById("addItem");
-// let headerTitle = document.getElementById("header-title");
-// let listItems = document.getElementsByClassName('list-group-item');
+let formBtn = document.querySelector('#addForm');
+let listItems = document.querySelector('#items');     //list
+let item = document.querySelector("#addItemInp");       //new item input
 
-// listItems[2].style.backgroundColor='green';
-// for(let i of listItems){
-//     i.style.fontWeight='bold';
-// }
-// // header.style.borderBottom="2px solid black";
-// headerTitle.style.borderBottom="2px solid black";
-// addItem.style.fontWeight="bold";
-// addItem.style.color="green";
-// let testClassLi = document.getElementsByClassName('test-class');
-// testClassLi[0].style.backgroundColor='red';
-// let li=document.getElementsByTagName('li');
-// for(let i of li){
-//     i.style.backgroundColor='yellow';
-// }
+formBtn.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(item.value!='')
+    {
+        let newItemLi = document.createElement('li');
+        newItemLi.className='list-group-item';
+        newItemLi.append(document.createTextNode(item.value));
 
-// let listItems = document.querySelector('.list-group-item');
-// let listItem2nd = document.querySelector('.list-group-item:nth-child(2)');
-// let listItem3rd = document.querySelector('.list-group-item:nth-child(3)');
+        let deleteBtn = document.createElement('button');
+        deleteBtn.className='btn btn-danger float-right delete';
+        deleteBtn.append(document.createTextNode('X'));
+        newItemLi.append(deleteBtn);
+        
 
-// listItem2nd.style.backgroundColor='green';
-// listItem3rd.style.display='none';
+        let editBtn = document.createElement('button');
+        editBtn.className='mx-1 btn btn-info float-right edit';
+        editBtn.append(document.createTextNode('Edit'));
+        newItemLi.append(editBtn);
 
+        listItems.appendChild(newItemLi);
+        item.value='';
+    }
+});
 
-// let listItems = document.querySelectorAll('.list-group-item');
-// listItems[1].style.color='green';
-// let odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
-// for(let i of odd){
-//     i.style.backgroundColor='green';
-// }
-
-let item=document.querySelector('#items');
-//parentElement
-item.parentElement.style.backgroundColor='#f4f4f4';
-//lastelementchild
-item.lastElementChild.style.color='red';
-//lastchild
-console.log(item.lastChild);
-//create child
-let newLi=document.createElement('li');
-newLi.innerText='item6';
-item.appendChild(newLi);
-//firstelementchild
-item.firstElementChild.style.backgroundColor='yellow';
-//firstchild
-console.log(item.firstChild);
-//nextsibling
-console.log(item.nextSibling);
-//nextelementsibling
-item.nextElementSibling.style.borderBottom='2px solid black';
-//previoussibling
-console.log(item.previousSibling);
-//previouselementsibling
-item.previousElementSibling.style.borderTop='2px solid black';
-//create element
-let newDiv=document.createElement('div');
-//setAttribute
-newDiv.setAttribute('title','test');
-//createtextnode
-let divText=document.createTextNode('Hello world');
-newDiv.appendChild(divText);
-
-//adding hello before item lister
-let head=document.querySelector('#main-header .container');
-let h1=document.querySelector('header h1');
-head.insertBefore(newDiv,h1);
-//adding hello world before list
-item.parentElement.insertBefore(newDiv,item);
+listItems.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
