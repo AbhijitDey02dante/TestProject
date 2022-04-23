eb8ac01df191005dc7049f2497e9ad565077c4a7
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-23 20:36:34 +0530

    BST Min Max

--- test.js
+++ test.js
@@ -1,91 +1,43 @@
-class Queue{
-    constructor(size){
-        this.queue=[];
-        this.size=size;
-        this.minPos=0;
-        this.maxPos=0;
-    }
-    enqueue(val){                           //insert value
-        if(this.maxPos<this.size)
-        {
-            this.queue[this.maxPos]=val;
-            this.maxPos=this.maxPos+1;
-        }
-        else
-        {
-            console.log('Full');
-        }
-    }
-
-    dequeue(){                               //remove value
-        if(this.minPos<this.maxPos)
-        {
-            let data=this.queue[this.minPos];
-            this.queue[this.minPos]=undefined;
-            this.minPos=this.minPos+1;
-            return data;
-        }
-        else
-            console.log('Empty');
-    }
-
-    peek(){                                 //check last element entered
-        if(this.minPos<this.maxPos)
-            return this.queue[this.maxPos-1];
-        else
-            console.log('Empty');
-    }
-
-    isEmpty(){                            //check if queue is empty
-        if(!(this.minPos<this.maxPos))
-            return true;
-        return false;
-    }
-
-    isFull(){                                  //check if queue is full
-        if(this.maxPos>=this.size)
-            return true;
-        return false;
-    }
-
-    display(){                                //display queue elements
-        for(let i=this.minPos;i<this.maxPos;i++)
-            console.log(this.queue[i]);
+class Node{
+    constructor(data){
+        this.data=data;
+        Node.left=null;
+        Node.right=null;
     }
 }
 
-
-class Stack{
-    constructor(size){
-        this.q1=new Queue(1000)
-        this.q2=new Queue(1000)
-        this.stArr=[];
-        this.size=size;
+function Insert(node , value){                                           //insertion
+    if(node==undefined || node.data==undefined){
+        let newNode=new Node(value);
+        return newNode;
     }
-    push(val){
-        if(this.stArr.length<this.size)
-            this.q1.enqueue(val);
-        else
-            console.log('Stack is full');
+    if(node.data<value){
+        node.right=Insert(node.right,value);
     }
-    pop(){
-        if(!(this.q1.isEmpty())){
-            let x=this.q1.dequeue();
-            while(!(this.q1.isEmpty())){
-                this.q2.enqueue(x);
-                x=this.q1.dequeue();
-            }
-            while(!(this.q2.isEmpty()))
-            {
-                this.q1.enqueue(this.q2.dequeue());
-            }
-            return x;
-        }
+    else{
+        node.left=Insert(node.left,value);
     }
-    display(){
-        this.q1.display();
-    }
-
+    return node;
 }
-let st1=new Stack(3);
-// st1.display(4);
+
+function findMin(node){
+    if(node.left==undefined)
+        return node.data;
+    return findMin(node.left);
+}
+function findMax(node){
+    if(node.right==undefined)
+        return node.data;
+    return findMax(node.right);
+}
+
+let root=new Node();
+root=Insert(root,10);
+root=Insert(root,20);
+root=Insert(root,8);
+root=Insert(root,2);
+root=Insert(root,17);
+root=Insert(root,16);
+root=Insert(root,14);
+console.log(findMin(root));
+console.log(findMax(root));
