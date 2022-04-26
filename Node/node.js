28a6975b271726bb883777d9e999cb3a6d945183
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-26 22:51:26 +0530

    CleanUp code

--- Node/node.js
+++ Node/node.js
@@ -1,41 +1,6 @@
 const http = require('http');
-const fs=require('fs');
-
-const server=http.createServer((req,res)=>{
-    res.setHeader('Content-type','text/html');
-    const url=req.url;
-    const method=req.method;
-    if(url==='/'){
-        let message='';
-        try{
-            message=fs.readFileSync('message.txt');
-        }
-        catch{
-            message='';
-        }
-        res.write('<html>');
-        res.write('<head><title>Enter Data</title></head>');
-        res.write('<body>');
-        res.write(`<p>${message}</p>`);
-        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form>')
-        res.write('</body>');
-    }
-    if(url==='/message' && method==='POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-        })
-        req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
-        })
-
-        res.statusCode=302;
-        res.setHeader('Location','/');
-    }
-    res.write('</html>');
-    res.end();
-})
+const routeHandler=require('./routes.js');
+console.log(routeHandler.randomString);
+const server=http.createServer(routeHandler.handler);
 
 server.listen(4000);
