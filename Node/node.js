cc703857be1fdfcf083b1de5978ac80f27419d4d
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-26 13:07:14 +0530

    Request and Response

--- Node/node.js
+++ Node/node.js
@@ -1,7 +1,26 @@
 const http = require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log('Abhijit Dey');
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    if(req.url=="/"){
+        res.write('<head><title>Welcome</title></head>');
+        res.write('<body><h1>Welcome</h1></body>');
+    }
+    if(req.url=="/home"){
+        res.write('<head><title>Welcome home</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+    }
+    if(req.url=="/about"){
+        res.write('<head><title>Welcome to About Us page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+    }
+    if(req.url=="/node"){
+        res.write('<head><title>Welcome to my Node Js project</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    }
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(4000);
