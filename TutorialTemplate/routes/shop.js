695012869827088de179ec7dc92ddde9d4491006
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-04-28 15:04:39 +0530

    Dynamic Routes 1

--- TutorialTemplate/routes/shop.js
+++ TutorialTemplate/routes/shop.js
@@ -2,11 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController=require('../controllers/product');
-const adminData = require('./admin');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsController.showProduct);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productID',shopController.getIDProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
